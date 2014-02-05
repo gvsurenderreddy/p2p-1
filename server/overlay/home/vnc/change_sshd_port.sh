081fbd3e8f85a0a0d24f8f63a726e900dbdb9af2
Dashamir Hoxha <dashohoxha@gmail.com>
2014-02-05 06:59:27 +0100

    Removed port and key info from index.html

--- server/overlay/home/vnc/change_sshd_port.sh
+++ server/overlay/home/vnc/change_sshd_port.sh
@@ -15,11 +15,8 @@ sed -i /etc/ssh/sshd_config -e "/^Port/c Port $port"
 ### restart the service
 service ssh restart
 
-### update www data
-### if mini-httpd is running, clients can get the port from it
+### update www/port, clients get the port from it
 echo $port > /home/vnc/www/port
-sed -i /home/vnc/www/index.html \
-    -e "s#<strong>port=[0-9]\+</strong>#<strong>port=$port</strong>#"
 
 ### notify
 echo "
