19a702031b1d38931c985aaa4a1309cec2cca821
Dashamir Hoxha <dashohoxha@gmail.com>
2014-09-24 10:43:34 +0200

    Fixed mini-httpd configuration in a container.

--- install/install-container.sh
+++ install/install-container.sh
@@ -17,3 +17,9 @@ EOF
 ### run the script for installing the system
 export DEBIAN_FRONTEND=noninteractive
 $code_dir/install/install-and-config.sh
+
+### fix the configuration of mini-httpd
+### when it runs inside a container
+### the correct setting should be 'nochroot'
+sed -i /etc/mini-httpd.conf \
+    -e 's/^chroot/nochroot'
